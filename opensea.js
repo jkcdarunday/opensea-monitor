324672d77a1cad8bc587af758dfafd926d978ac2
Jan Keith Darunday <jkcdarunday@gmail.com>
2021-09-15 07:25:35 +0800

    Instead of simulating a click on Buy Now and listening to requests, directly parse the value of window.__wired__ which is generated by the SSR of OpenSea which reduces complexity and request count

--- opensea.js
+++ opensea.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer-extra';
+import _ from 'lodash';
 
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import sleep from 'sleep';
@@ -15,46 +16,37 @@ export async function search(url) {
     const page = await browser.newPage();
     await page.setViewport({ width: 1366, height: 768 });
 
-    const dataPromise = new Promise((resolve, reject) => {
-        page.on('response', async response => {
-            try {
-                const request = response.request();
-                if (request.url().includes('/graphql')
-                    && request.method() === 'POST'
-                    && request.postData().includes('AssetSearchQuery')) {
-                    const json = await response.json();
-                    resolve(json.data.query.search.edges.slice(0, 3)
-                        .map(asset => asset.node.asset));
-                }
-            } catch (error) {
-                reject(error);
-            }
-        });
-    });
-
     console.log('Loading page...');
     await page.goto(url);
-    sleep.sleep(1);
 
-    console.log('Toggling "Buy Now"...');
-    const [buyNowButton] = await page.$x("//div[contains(@class, 'FeaturedFilter--items')]/div[@class='FeaturedFilter--item' and contains(., 'Buy Now')]");
-    await buyNowButton.click();
-
-    console.log('Awaiting response...');
-    const response = await dataPromise;
+    const wiredRecords = JSON.parse(await page.evaluate(() => JSON.stringify(window.__wired__))).records;
     await browser.close();
 
-    console.log(`Data loaded (size: ${response.length})`);
-    return response;
-}
+    const searchResult = _(wiredRecords).values()
+        .find(wiredRecord => _.startsWith(wiredRecord.__id, 'client:root:query:search')
+            && wiredRecord.__typename === 'SearchTypeConnection');
 
-export function extractAsset(asset) {
-    return {
-        price: asset.orderData.bestAsk.paymentAssetQuantity.quantity / 1000000000000000000,
-        url: `https://opensea.io/assets/${asset.assetContract.address}/${asset.tokenId}`,
-    };
-}
+    const assets = _(searchResult.edges.__refs)
+        .map(edgeId => _.get(wiredRecords, edgeId))
+        .map(edge => _.get(wiredRecords, edge.node.__ref))
+        .map(edgeNode => _.get(wiredRecords, edgeNode.asset.__ref))
+        .map(asset => {
+            const orderData = _.get(wiredRecords, asset.orderData.__ref);
+            const bestAsk = _.get(wiredRecords, orderData.bestAsk.__ref);
+            const paymentAssetQuantity = _.get(wiredRecords, bestAsk.paymentAssetQuantity.__ref);
 
-export function extractPrice(asset) {
-    return asset.orderData.bestAsk.paymentAssetQuantity.quantity / 1000000000000000000;
+            const contract = _.get(wiredRecords, asset.assetContract.__ref);
+
+            const collection = _.get(wiredRecords, asset.collection.__ref);
+
+            return {
+                name: asset.name,
+                price: paymentAssetQuantity.quantity / 1_000_000_000_000_000_000,
+                collection: { name: collection.name },
+                url: `https://opensea.io/assets/${contract.address}/${asset.tokenId}`,
+            };
+        })
+        .value();
+
+    return assets;
 }
